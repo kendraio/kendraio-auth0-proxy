57fac571794af64aef025d0feb9adbae1827c6b2
Darren <darren@darrenmothersele.com>
2019-01-04 14:16:37 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Verify Auth0 ID Token and return full user profile with IDP tokens.
---
+++ index.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const cors = require('cors');
+
+// Create middleware for checking the JWT
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: `https://${process.env.DOMAIN}/api/v2/`,
+  issuer: `https://${process.env.DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+app.use(cors());
+app.use(checkJwt);
+
+
+app.use(require('body-parser').json());
+
+const axios = require('axios');
+
+async function getProfile(userId) {
+  const { data } = await axios({
+    method: 'post',
+    url: `https://${process.env.DOMAIN}/oauth/token`,
+    headers: { 'content-type': 'application/json' },
+    data: {
+      "client_id": process.env.CLIENT_ID,
+      "client_secret": process.env.CLIENT_SECRET,
+      "audience": `https://${process.env.DOMAIN}/api/v2/`,
+      "grant_type": "client_credentials"
+    }
+  });
+  const { access_token } = data;
+
+  const { data: profile } = await axios({
+    method: 'get',
+    url: `https://${process.env.DOMAIN}/api/v2/users/${ userId }`,
+    headers: { authorization: `Bearer ${ access_token }` }
+  });
+
+  return profile;
+}
+
+app.get('/', (req,res) => res.send('Hello'));
+app.post('/', (req, res) => {
+  const { userId } = req.body;
+  if (!userId) {
+    return res.status(400).send();
+  }
+  getProfile(userId).then(profile => res.json(profile));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
