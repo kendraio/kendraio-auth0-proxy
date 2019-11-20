155c516ec53ee8efb18648b05fd3e17c6f8fbda7
Darren <darren@darrenmothersele.com>
2019-11-20 09:36:58 +0000

    add error handling

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000;
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 const cors = require('cors');
+const axios = require('axios');
 
 // Create middleware for checking the JWT
 const checkJwt = jwt({
@@ -24,27 +25,43 @@ const checkJwt = jwt({
 });
 
 app.use(cors());
-
 app.use(require('body-parser').json());
 
-app.post('/refresh', (req, res) => {
-  const { refreshToken } = req.body;
+app.post('/refresh', async (req, res) => {
+  const {refreshToken} = req.body;
   if (!refreshToken) {
     return res.status(400).send();
   }
-  getFromRefreshToken(refreshToken).then(result => res.json(result));
+  try {
+    const result = await getFromRefreshToken(refreshToken);
+    return res.json(result);
+  } catch (e) {
+    return res.status(403).send({message: e.message});
+  }
 });
 
-app.use(checkJwt);
+app.get('/', (req, res) => res.send('Hello'));
 
+app.post('/', checkJwt, async (req, res) => {
+  if (!req.user.sub) {
+    return res.status(400).send();
+  }
+  try {
+    const profile = await getProfile(req.user.sub);
+    return res.json(profile);
+  } catch (e) {
+    console.log(e);
+    return res.status(403).send({message: e.message});
+  }
+});
+app.listen(port, () => console.log(`Listening on port ${port}!`));
 
-const axios = require('axios');
 
 async function getProfile(userId) {
-  const { data } = await axios({
+  const {data} = await axios({
     method: 'post',
     url: `https://${process.env.DOMAIN}/oauth/token`,
-    headers: { 'content-type': 'application/json' },
+    headers: {'content-type': 'application/json'},
     data: {
       "client_id": process.env.CLIENT_ID,
       "client_secret": process.env.CLIENT_SECRET,
@@ -52,22 +69,22 @@ async function getProfile(userId) {
       "grant_type": "client_credentials"
     }
   });
-  const { access_token } = data;
+  const {access_token} = data;
 
-  const { data: profile } = await axios({
+  const {data: profile} = await axios({
     method: 'get',
-    url: `https://${process.env.DOMAIN}/api/v2/users/${ userId }`,
-    headers: { authorization: `Bearer ${ access_token }` }
+    url: `https://${process.env.DOMAIN}/api/v2/users/${userId}`,
+    headers: {authorization: `Bearer ${access_token}`}
   });
 
   return profile;
 }
 
 async function getFromRefreshToken(refreshToken) {
-  const { data } = await axios({
+  const {data} = await axios({
     method: 'post',
     url: `https://www.googleapis.com/oauth2/v4/token`,
-    headers: { 'content-type': 'application/json' },
+    headers: {'content-type': 'application/json'},
     data: {
       "client_id": process.env.G_ID,
       "client_secret": process.env.G_SECRET,
@@ -77,15 +94,3 @@ async function getFromRefreshToken(refreshToken) {
   });
   return data;
 }
-
-app.get('/', (req,res) => res.send('Hello'));
-app.post('/', (req, res) => {
-  const { userId } = req.body;
-  if (!userId) {
-    return res.status(400).send();
-  }
-  getProfile(userId).then(profile => res.json(profile));
-});
-
-
-app.listen(port, () => console.log(`Listening on port ${port}!`));
